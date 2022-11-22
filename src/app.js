bf3339d4f9bf908635dc404319bee9389d5730b3
shetadhruvik <shetadhruvik13@gmail.com>
2022-11-22 15:54:31 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,3 +4,5 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/DDatabase")
 .then(()=>{console.log('connection okay');})
 .catch((err)=>{console.log(err);});
+
+var a = 121
